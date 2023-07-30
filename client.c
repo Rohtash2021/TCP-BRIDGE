1224221595ebf1bffd10eb3dd00def471117f60d
Rohtash2021 <125363372+Rohtash2021@users.noreply.github.com>
2023-07-30 11:28:56 +0530

    Client.c

--- client.c
+++ client.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#define FILE_TO_SEND "file.txt"
+
 int main(){
 
   char *ip = "127.0.0.1";
@@ -15,12 +17,14 @@ int main(){
   char buffer[1024];
   int n;
 
+  FILE *file;
+
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0){
-    perror("[-]Socket error");
+    perror("Socket error");
     exit(1);
   }
-  printf("[+]TCP server socket created.\n");
+  printf("TCP server socket created.\n");
 
   memset(&addr, '\0', sizeof(addr));
   addr.sin_family = AF_INET;
@@ -30,18 +34,36 @@ int main(){
   connect(sock, (struct sockaddr*)&addr, sizeof(addr));
   printf("Connected to the server.\n");
 
-  bzero(buffer, 1024);
-  strcpy(buffer, "HELLO, THIS IS CLIENT.");
-  printf("Client: %s\n", buffer);
-  send(sock, buffer, strlen(buffer), 0);
+  while (1) {
+    printf("Enter 'send' to send the file contents to the server: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\r\n")] = 0; 
 
-  bzero(buffer, 1024);
-  recv(sock, buffer, sizeof(buffer), 0);
-  printf("Server: %s\n", buffer);
+    if (strcmp(buffer, "send") == 0) {
+      file = fopen(FILE_TO_SEND, "r");
+      if (file == NULL) {
+        perror("File open error");
+        continue;
+      }
+
+      bzero(buffer, 1024);
+      ssize_t bytes_read = fread(buffer, 1, sizeof(buffer) - 1, file);
+      if (bytes_read <= 0) {
+        perror("File read error");
+        fclose(file);
+        continue;
+      }
+
+      printf("Client: Sending file contents...\n");
+      send(sock, buffer, bytes_read, 0);
+      printf("Client: File contents sent to the server.\n");
+
+      fclose(file);
+    }
+  }
 
   close(sock);
   printf("Disconnected from the server.\n");
 
   return 0;
-
 }
